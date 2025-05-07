55c09f00e83e110a1f6313a23470b24aaf8c92b7
Lautaro Acosta Quintana <acostaquintanalautaro@gmail.com>
2025-05-07 13:52:31 -0300

    first commit

---
+++ src/scripts/with-rust-pre.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eux
+
+sscache --version
+command -v mold
+
+export SCCACHE_IDLE_TIMEOUT="0"
+export RUST_LOG="sccache=info"
+export SCCACHE_ERROR_LOG=/tmp/sccache.log
+export SCCACHE_LOG="info,sccache::cache=debug"
+sccache --start-server
+
+rustflags=(
+  "-C link-arg=-fuse-ld=mold"
+  "-C link-arg=-Wl,--compress-debug-sections=zlib"
+  "-C force-frame-pointers=yes"
+)
+
+cat << EOF >> "${BASH_ENV}"
+  export RUSTC_WRAPPER="sccache"
+  export CARGO_INCREMENTAL="0"
+  export CARGO_PROFILE_RELEASE_LTO="thin"
+  export RUSTFLAGS="${rustflags[*]}"
+EOF
